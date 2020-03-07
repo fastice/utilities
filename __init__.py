698727afe6db4fa68b701a7d3498040ec8933f8a
Ian Joughin <ian@apl.washington.edu>
2020-03-07 10:03:23 -0800

    moved argusMesh.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
-__all__ = ['dols', 'strip','popd','pushd','readImage','writeImage','readLLtoRA','writeLLtoRAformat',
-           'myerror','runMyThreads','mywarning','myalert','hsvVelCmap','getWKT_PROJ','argusMesh','myPrompt','callMyProg','logger', 'processProfile']
+__all__ = ['dols', 'strip', 'popd', 'pushd', 'readImage', 'writeImage',
+           'readLLtoRA', 'writeLLtoRAformat', 'myerror', 'runMyThreads',
+           'mywarning', 'myalert', 'hsvVelCmap', 'getWKT_PROJ', 'myPrompt',
+           'callMyProg', 'logger', 'processProfile', 'runvel', 'geoimage',
+           'geodat', 'geodatrxa', 'writeLLtoRAformat', 'readLLtoRA',
+           'offsets', 'lsdat', 'lsfit', 'makeMaskFromShape', 'getWKT_PROJ',
+           'shpplot']
 from utilities.dols import dols
 from utilities.myerror import myerror
 from utilities.strip import strip
@@ -12,17 +17,16 @@ from utilities.geodatrxa import geodatrxa
 from utilities.offsets import offsets
 from utilities.readImage import readImage
 from utilities.writeImage import writeImage
-from utilities.readwriteLLtoRA import writeLLtoRAformat,readLLtoRA
+from utilities.readwriteLLtoRA import writeLLtoRAformat, readLLtoRA
 from utilities.runMyThreads import runMyThreads
 from utilities.lsdat import lsdat
 from utilities.lsfit import lsfit
 from utilities.mywarning import mywarning
 from utilities.myalert import myalert
-from utilities.makeMaskFromShape import makeMaskFromShape 
-from utilities.getWKT_PROJ import getWKT_PROJ 
+from utilities.makeMaskFromShape import makeMaskFromShape
+from utilities.getWKT_PROJ import getWKT_PROJ
 from utilities.shpplot import shpplot
 from utilities.hsvVelCmap import hsvVelCmap
-from utilities.argusMesh import argusMesh
 from utilities.myPrompt import myPrompt
 from utilities.callMyProg import callMyProg
 from utilities.logger import logger
